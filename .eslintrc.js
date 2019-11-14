006de7466c684dede4ac7067586f25d8444d5e87
Tory <tory37@gmail.com>
2019-11-14 01:45:33 -0600

    Added babel-eslint to allow for eslintrc inclusion

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,14 @@
 module.exports = {
-  rules: {
-      'no-console': 'off',
+  root: true,
+  env: {
+    node: true
   },
+  extends: ["plugin:vue/essential",'plugin:prettier/recommended', "@vue/prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
 };
